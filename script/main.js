5f0df2a17bed564eb534d8dd6c96a453c62d28ab
Nishi Shah <nishis.kiveesoftech@gmail.com>
2020-12-02 10:07:18 +0530

    indoor-page

--- script/main.js
+++ script/main.js
@@ -45,5 +45,22 @@ $(".btn-outline-dark").click(function(){
     counter++;
     $("#counter").text(counter);
     });
+  
+    $('.carousel.carousel-multi .item').each(function () {
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
+    
+      if (next.next().length > 0) {
+        next.next().children(':first-child').clone().attr("aria-hidden", "true").appendTo($(this));
+      }
+      else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+      }
+    });
 
 }); 
+
+
